24aa19b5f91d7969e3ed1b8a511ab5829ad9943f
Ghaith <ghayth.1900@gmail.com>
2025-11-01 00:36:07 +0100

    finish  the task  model

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -1,12 +1,58 @@
 class TaskModel {
-  int id;
-  String title;
-  String description;
-  bool iscompleted;
+  final int id;
+  final String title;
+  final String description;
+  bool isCompleted;
+  final double? latitude;
+  final double? longitude;
+
   TaskModel({
     required this.id,
     required this.title,
     required this.description,
-    this.iscompleted = false,
+    this.isCompleted = false,
+    this.latitude,
+    this.longitude,
   });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) {
+    return TaskModel(
+      id: json['id'] ?? 0,
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
+      isCompleted: json['isCompleted'] ?? false,
+      latitude: (json['latitude'] != null) ? json['latitude'].toDouble() : null,
+      longitude:
+          (json['longitude'] != null) ? json['longitude'].toDouble() : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'isCompleted': isCompleted,
+      'latitude': latitude,
+      'longitude': longitude,
+    };
+  }
+
+  TaskModel copyWith({
+    int? id,
+    String? title,
+    String? description,
+    bool? isCompleted,
+    double? latitude,
+    double? longitude,
+  }) {
+    return TaskModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isCompleted: isCompleted ?? this.isCompleted,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
+    );
+  }
 }
