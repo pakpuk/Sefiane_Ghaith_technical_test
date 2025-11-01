27914046ce4060865b244ed4b9638e463f06dbca
Ghaith <ghayth.1900@gmail.com>
2025-11-01 03:38:23 +0100

    almost finish home screeen

--- lib/presentation/widgets/task_widget.dart
+++ lib/presentation/widgets/task_widget.dart
@@ -12,42 +12,45 @@ class TaskWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final taskProvider = Provider.of<TaskProvider>(context, listen: false);
-    return Container(
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
       margin: const EdgeInsets.all(20),
       padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           border: Border.all(color: ColorsManager.textfieldbordercolor)),
-      child: ListTile(
+      child: InkWell(
         onTap: () {
           Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) => const TaskDetailScreen()));
         },
-        title: Text(
-          task.title,
-          style: TextStyles.font13bold.copyWith(
-              decoration: task.isCompleted
-                  ? TextDecoration.lineThrough
-                  : TextDecoration.none),
-        ),
-        subtitle: Text(
-          task.description,
-          style: TextStyles.smallText,
-        ),
-        trailing: IconButton(
-          onPressed: () {
-            taskProvider.toggleTaskCompletion(task.id);
-          },
-          icon: task.isCompleted
-              ? const Icon(Icons.check_circle_outline_outlined)
-              : const Icon(
-                  Icons.circle_outlined,
-                ),
-          color: task.isCompleted
-              ? ColorsManager.greenColor
-              : ColorsManager.greycolor,
+        child: ListTile(
+          title: Text(
+            task.title,
+            style: TextStyles.font13bold.copyWith(
+                decoration: task.isCompleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none),
+          ),
+          subtitle: Text(
+            task.description,
+            style: TextStyles.smallText,
+          ),
+          trailing: IconButton(
+            onPressed: () {
+              taskProvider.toggleTaskCompletion(task.id);
+            },
+            icon: Icon(
+              task.isCompleted
+                  ? Icons.check_circle
+                  : Icons.radio_button_unchecked,
+              color: task.isCompleted
+                  ? ColorsManager.greenColor
+                  : ColorsManager.greycolor,
+            ),
+          ),
         ),
       ),
     );
